1eeb01d6312455bc11efc1535e15b3e0941f3d9d
ThamizhAnban <thamizhanban2006@gmail.com>
2025-01-27 18:15:33 +0530

    Milestone-4

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 const ErrorHandler = require("./middleware/error");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser")
+
+app.use(express.json())
+app.use(cookieParser())
+app.use("/",express.static("uploads"));
+app.use(bodyParser.urlencoded({extended:true,limit:"50mb"}))
 
 //config
 if(process.env.NODE_ENV !== "PRODUCTION"){
@@ -8,5 +15,9 @@ if(process.env.NODE_ENV !== "PRODUCTION"){
         path:"backend/config/.env",
     });
 }
+
+const user = require("./controller/user");
+app.use("/api/v2/user",user);
+
 app.use(ErrorHandler);
 module.exports=app;
